045ff4d9ef810919dd2a430fbfa719d47b97d8af
coderaiser <coderaiser@cloudcmd.io>
2023-08-03 19:09:16 +0300

    fix: @putout/babel: traverse

--- babel/index.js
+++ babel/index.js
@@ -1,11 +1,12 @@
 export * from '@babel/parser';
-export * from '@babel/traverse';
 export * from '@babel/template';
 export * from '@babel/generator';
 export * from '@babel/code-frame';
 
 import * as types from '@babel/types';
+import traverse from '@babel/traverse';
 
 export {
     types,
+    traverse,
 }
