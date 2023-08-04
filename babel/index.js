36b095a83c9bf4298f756a73cc54920e0d9080e6
coderaiser <coderaiser@cloudcmd.io>
2023-08-04 11:13:25 +0300

    fix: @putout/babel: template

--- babel/index.js
+++ babel/index.js
@@ -1,7 +1,5 @@
-export * from '@babel/parser';
-export * from '@babel/template';
+import template from '@babel/template';
 import generate from '@babel/generator';
-export * from '@babel/code-frame';
 
 import * as types from '@babel/types';
 import traverse from '@babel/traverse';
@@ -10,4 +8,8 @@ export {
     types,
     traverse,
     generate,
+    template,
 }
+
+export * from '@babel/parser';
+export * from '@babel/code-frame';
