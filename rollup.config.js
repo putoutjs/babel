98b0bec50b2a5db4082e964efcce9e1d48f17c8a
coderaiser <coderaiser@cloudcmd.io>
2023-09-26 18:38:27 +0300

    fix: navigator

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import {nodeResolve} from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import alias from '@rollup/plugin-alias';
 import externals from 'rollup-plugin-node-externals';
+import replace from '@rollup/plugin-replace';
 
 export default {
     input: 'babel/index.js',
@@ -23,5 +24,13 @@ export default {
         }),
         commonjs(),
         json(),
+        replace({
+            exclude: 'globals',
+            preventAssignment: true,
+            values: {
+                'navigator.userAgentData': 'globalThis?.navigator?.userAgentData',
+                'navigator.userAgent': 'globalThis?.navigator?.userAgent',
+            }
+        }),
     ],
 };
