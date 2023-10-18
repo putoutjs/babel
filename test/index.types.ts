38dcdce83019bf449e08c17d5be8c92b40ac3e6a
coderaiser <coderaiser@cloudcmd.io>
2023-10-18 19:46:10 +0300

    chore: @putout/babel

--- test/index.types.ts
+++ test/index.types.ts
@@ -1,1 +1,16 @@
+import {codeFrameColumns} from '..';
 
+const rawLines = `class Foo {
+  constructor()
+}`;
+
+const location = {
+    start: {
+        line: 2,
+        column: 16,
+    },
+};
+
+const result = codeFrameColumns(rawLines, location, {/* options */});
+
+console.log(result);
