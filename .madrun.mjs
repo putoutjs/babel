3bfa2cca10dcb49156bda3c34c28dac2dfae8f3e
coderiaser <coderaiser@cloudcmd.io>
2025-02-15 12:37:23 +0200

    fix: test:dts

--- .madrun.mjs
+++ .madrun.mjs
@@ -1,7 +1,7 @@
 import {run} from 'madrun';
 
 export default {
-    'test': () => run(['build', 'test:js']),
+    'test': () => run(['build', 'test:*']),
     'test:js': () => `tape 'lib/*.spec.js'`,
     'test:dts': () => 'check-dts test/*.ts',
     'watch:test': async () => `nodemon -w lib -w test -x ${await run('test')}`,
@@ -14,7 +14,7 @@ export default {
     'wisdom': () => run('test'),
     'clean': () => 'rm -rf bundle',
     'build': () => run(['clean', 'build:js', 'build:fix']),
-    'build:js': () => 'tsup --target es5 lib/index.mjs -d bundle --dts-resolve --dts lib/index.d.ts',
-    'build:types': () => 'tsup --target es5 lib/index.d.ts -d bundle --dts-resolve --dts-only',
+    'build:js': () => 'tsup lib/index.mjs -d bundle --dts-resolve --dts lib/index.d.ts',
+    'build:types': () => 'tsup lib/index.d.ts -d bundle --dts-resolve --dts-only',
     'build:fix': () => `mv bundle/index.d.d.ts bundle/index.d.ts`,
 };
--- test/index.errors.ts
+++ test/index.errors.ts
@@ -9,10 +9,10 @@ const {isIdentifier} = types;
 
 // THROWS Expected 1-2 arguments, but got 3
 isIdentifier(1, 2, 3);
-// THROWS Type 'boolean' is not assignable to type 'number'
-const a: number = isIdentifier({});
+// THROWS Argument of type '{}' is not assignable to parameter of type 'Node'
+const a = isIdentifier({});
 
-// THROWS Argument of type 'number' is not assignable to parameter of type 'string'.
+// THROWS Argument of type 'boolean' is not assignable to parameter of type 'string'
 parse(a);
 // THROWS Expected 3 arguments, but got 0.
 codeFrameColumns();
