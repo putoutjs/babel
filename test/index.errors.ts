2d71963fba071946c8714bb5adcae09fd12c600b
coderiaser <coderaiser@cloudcmd.io>
2025-05-31 14:24:31 +0300

    chore: types

--- test/index.errors.ts
+++ test/index.errors.ts
@@ -9,7 +9,7 @@ const {isIdentifier} = types;
 
 // THROWS Expected 1-2 arguments, but got 3
 isIdentifier(1, 2, 3);
-// THROWS Argument of type 'string' is not assignable to parameter of type 'object'
+// THROWS Argument of type 'string' is not assignable to parameter of type 'Node'
 const a = isIdentifier('x');
 
 // THROWS Argument of type 'boolean' is not assignable to parameter of type 'string'
