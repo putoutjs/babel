e60bc97e55f7f29ecd7fdc8141ac15e03afc5bce
coderiaser <coderaiser@cloudcmd.io>
2025-09-09 23:36:35 +0300

    feature: @putout/babel: decrease bundle size: rm flow

--- rules/remove-flow.js
+++ rules/remove-flow.js
@@ -15,6 +15,12 @@ module.exports.report = () => 'Remove flow';
 
 module.exports.replace = () => ({
     'var flow = __': '',
+    'var FlowErrors = __': '',
+    'function isFlowType(__args) {__body}': 'function isFlowType() {}',
+    'function isFlow(__args) {__body}': 'function isFlow() {}',
+    'function isFlowBaseAnnotation(__args) {__body}': 'function isFlowBaseAnnotation() {}',
+    'function isFlowDeclaration(__args) {__body}': 'function isFlowDeclaration() {}',
+    'function isFlowPredicate(__args) {__body}': 'function isFlowPredicate() {}',
     [MIXIN]: `
     var mixinPlugins = {
         estree,
