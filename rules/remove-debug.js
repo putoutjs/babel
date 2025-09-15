2f102748ab5f4120531c1c0c09086db7b336d522
coderiaser <coderaiser@cloudcmd.io>
2025-09-15 17:34:47 +0300

    fix: @putout/babel: debug

--- rules/remove-debug.js
+++ rules/remove-debug.js
@@ -1,21 +1,14 @@
 'use strict';
 
-const MIXIN = `
-  var mixinPlugins = {
-      estree,
-      jsx,
-      flow,
-      typescript,
-      v8intrinsic,
-      placeholders,
-  };
-`;
+const FN = `() => () => ({
+    enabled: false,
+})`;
 
 module.exports.report = () => 'Remove flow';
 
 module.exports.replace = () => ({
-    'var require_common = __': 'var require_common = () => () => {}',
-    'var require_browser = __': 'var require_browser = () => () => {}',
-    'var require_ms = __': 'var require_ms = () => () => {}',
-    'var require_node = __': 'var require_node = () => () => {}',
+    'var require_common = __': `var require_common = ${FN}`,
+    'var require_browser = __': `var require_browser = ${FN}`,
+    'var require_ms = __': `var require_ms = () => () => {}`,
+    'var require_node = __': `var require_node = ${FN}`,
 });
