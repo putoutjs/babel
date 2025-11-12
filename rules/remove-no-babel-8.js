5c2b49d8766566c5edeb4efb7a42af8c83bce7e4
coderiaser <coderaiser@cloudcmd.io>
2025-11-12 12:29:07 +0200

    feature: babel: decrease bundle size: no babel 8 check

--- rules/remove-no-babel-8.js
+++ rules/remove-no-babel-8.js
@@ -3,6 +3,6 @@
 module.exports.report = () => 'Remove no BABEL';
 
 module.exports.replace = () => ({
-    'var require_js_tokens_BABEL_8_BREAKING_false = __': 'var require_js_tokens_BABEL_8_BREAKING_false = () => {}',
+    'var require_js_tokens_BABEL_8_BREAKING_false = __': '',
     [`module2.exports = bool2(process.env['BABEL_8_BREAKING']) ? require_js_tokens_BABEL_8_BREAKING_true() : require_js_tokens_BABEL_8_BREAKING_false();`]: 'module2.exports = require_js_tokens_BABEL_8_BREAKING_true()',
 });
