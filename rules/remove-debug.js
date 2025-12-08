0926989e389878d01cf9673f574e84591b5876d9
coderiaser <coderaiser@cloudcmd.io>
2025-12-08 22:46:42 +0200

    fix: babel: obug: enabled

--- rules/remove-debug.js
+++ rules/remove-debug.js
@@ -15,7 +15,11 @@ module.exports.replace = () => ({
         inspect: () => false,
     }`,
     'var colors = process.stderr.getColorDepth && process.stderr.getColorDepth() > 2 ? __a : __b': 'var colors = []',
-    'function createDebug(__args) {__body}': 'function createDebug() {}',
+    'function createDebug(__args) {__body}': `function createDebug() {
+        return {
+            enabled: false,
+        }
+    }`,
     'var inspectOpts = __': 'var inspectOpts = {}',
     'function formatArgs(__args) {__body}': 'function formatArgs() {}',
     [USE_COLORS]: 'function useColors() {}',
