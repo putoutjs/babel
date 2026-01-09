fe520268da0f42e42973836afadda74f480d7aaf
coderiaser <coderaiser@cloudcmd.io>
2026-01-09 23:38:33 +0200

    chore: scripts: build-babel: update

--- scripts/build-babel.sh
+++ scripts/build-babel.sh
@@ -5,8 +5,7 @@ git reset HEAD~ —hard
 git tag -d tmp.v8.0.0-beta.3
 git pull --rebase
 
-~/putout/packages/putout/bin/putout.js --no-config Makefile.source.mjs -t 'if (name === "babel-eslint-plugin") __body -> if (name === "babel-eslint-plugin" && babel8Condition) __body' --f none --fix && echo '✅ 'Makefile.source' fixed'
 Make build prepublish
-node Makefile.source.mjs new-babel-8-version-create-commit-ci
+node Makefile.source.mjs prepublish
 
 cd ../babel
