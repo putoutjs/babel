e2fbcfd5e17a0f304d0f157d93b1197de48b5cf2
coderaiser <coderaiser@users.noreply.github.com>
2026-07-09 17:02:30 +0000

    chore: @putout/babel: actions: lint ☘️

--- test/errors.ts
+++ test/errors.ts
@@ -8,6 +8,7 @@ const {identifier, isIdentifier} = types;
 
 // THROWS Expected 1-2 arguments, but got 3
 isIdentifier(1, 2, 3);
+
 // THROWS No overload matches this call.
 const a = isIdentifier('x');
 
@@ -15,5 +16,6 @@ const node: Node = identifier('hello');
 
 // THROWS '"hello"' has no properties in common with type 'Partial<{ name: string; decorators: Decorator[] | null | undefined; optional: boolean | null | undefined; typeAnnotation: TSTypeAnnotation | TypeAnnotation | null | undefined; ... 7 more ...; extra: Record<...> | undefined; }>'.
 isIdentifier(node, 'hello');
+
 // THROWS Argument of type 'boolean' is not assignable to parameter of type 'string'
 parse(a);
